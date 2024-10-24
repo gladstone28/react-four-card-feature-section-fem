208403ca1559e2e46c25b5d9a668a5a2132c1012
Gladstone Rose <gladstonerose@hotmail.com>
2024-10-24 08:58:16 -0500

    modify index.html and index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a root.
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component to the root.
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
